bb9e9283c885619e5aca0fd829b2cbc57f3271b5
MFrogy24 <86326555+MFrogy24@users.noreply.github.com>
2022-07-26 18:59:05 -0600

    Update smartiot02.py
    def print_info() added

--- smartiot02.py
+++ smartiot02.py
@@ -4,7 +4,7 @@ import torchaudio
 
 def version():
   '''Shows Smart IoT library version'''
-  print('Smart Iot Library ver. 1.1')
+  print('Smart Iot Library ver. 1.2')
 
 def load_audio(url, fname):
   '''Regresa la señal de audio, sample-rate- metada, bytes-size'''
@@ -15,3 +15,22 @@ def load_audio(url, fname):
   meta= torchaudio.info(fname)
   wave, sr = torchaudio.load(fname)
   return (wave, sr, meta, sz)
+
+def print_info(info, fname=None):
+  '''Muestra los metadatos de un archivo de audio con la tupla que regresa load_audio'''
+  if fname:
+    print('-' * 30)
+    #print('Filename: ' fname)
+    print('-' * 30)
+  wave, sr, meta, sz = info 
+  channels, frames = wave.shape
+  print(f'           Frames: {frames}')
+  print(f'         Channels: {channels}')
+  print(f'  Audio File size: {sz} bytes')
+  print(f'      Tensor size: {wave.element_size() * meta.num_channels * meta.num_frames} bytes')
+  print(f'            Dtype: {wave.dtype}' )
+  print(f'              Max: {wave.max().item():6.3f}')
+  print(f'              Min: {wave.min().item():6.3f}')
+  print(f'             Mean: {wave.mean().item():6.3f}')
+  print(f'          Std Dev: {wave.std().item():6.3f}')
+  print(wave)
