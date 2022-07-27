3f0ee1bc7b29bd7a71905cf96702318e694879d8
MFrogy24 <86326555+MFrogy24@users.noreply.github.com>
2022-07-26 18:37:07 -0600

    Update smartiot02.py
    def load_audio() added

--- smartiot02.py
+++ smartiot02.py
@@ -1,5 +1,17 @@
 # Smart IoT Library 
+import requests
+import torchaudio
 
 def version():
   '''Shows Smart IoT library version'''
   print('Smart Iot Library ver. 1.0')
+
+def load_audio(url, fname):
+  '''Regresa la señal de audio, sample-rate- metada, bytes-size'''
+  r = requests.get(url)
+  with open(fname, 'wb') as f:
+    f.write(r.content)
+  sz = len(r.content)
+  meta= torchaudio.info(fname)
+  wave, sr = torchaudio.load(fname)
+  return (wave, sr, meta, sz)
